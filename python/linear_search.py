42324b064cb8b38f98ba43593cb97c58fe1868df
kayleeburch <kayleemburch@gmail.com>
2022-05-25 16:49:22 -0400

    python solutions

--- python/linear_search.py
+++ python/linear_search.py
@@ -2,5 +2,27 @@ array_to_search_through = []
 for number in range(1, 1001):
     array_to_search_through.append(number)
 
-def linear_search(value_to_find, array_to_search_through):
-    # your code here
+#release 0
+# def linear_search(value_to_find, array_to_search_through):
+#     count = -1
+#     for i in array_to_search_through:
+#         count += 1;
+#         if i == value_to_find:
+#             return count
+    
+#     return None
+
+#release 1
+def global_linear_search(value_to_find, array_to_search_through):
+    occur = []
+    count = -1
+    for i in array_to_search_through:
+        count += 1;
+        if i == value_to_find:
+            occur.append(count)
+
+    if(len(occur) == 0):
+        return None
+    
+    return occur
+
--- python/linear_search_spec.py
+++ python/linear_search_spec.py
@@ -1,5 +1,9 @@
-from linear_search import linear_search
+from linear_search import global_linear_search
 
-print(linear_search(3, [1,2,3]) == 2)
-print(linear_search(4, [1,2,3]) == None)
-print(linear_search(13, [1,2,3]) == None)
+print(global_linear_search(3, [1,2,3,3,6,7,3]) == [2,3,6])
+print(global_linear_search(4, [1,2,3]) == None)
+print(global_linear_search(13, [13,2,3]) == [0])
+print(global_linear_search(0, [0,1,2,3,0]) == [0,4])
+print(global_linear_search("a", ["b", "a", "n", "a", "n", "a", "s"]) == [1,3,5])
+print(global_linear_search(True, [True, False, True, False]) == [0,2])
+print(global_linear_search("?", ["?", ">", ">", "?", "*", 2, True]) == [0,3])
